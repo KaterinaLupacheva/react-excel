7de5a93588833ca41b577a9df089c41cdb285f49
Katerina <katerinalupacheva@gmail.com>
2020-05-24 23:52:05 +0300

    Updates

---
+++ precache-manifest.be706df6c3056d4f7e2f028cace2c503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48e523140c7c76888ee0c7c6cb28f79",
+    "url": "/@ramonak/react-excel/index.html"
+  },
+  {
+    "revision": "84301d9bd8a9e7b6d962",
+    "url": "/@ramonak/react-excel/static/css/main.237dba81.chunk.css"
+  },
+  {
+    "revision": "475cb759f650234305b7",
+    "url": "/@ramonak/react-excel/static/js/2.59ae92cf.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/@ramonak/react-excel/static/js/2.59ae92cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84301d9bd8a9e7b6d962",
+    "url": "/@ramonak/react-excel/static/js/main.cbb20427.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/@ramonak/react-excel/static/js/main.cbb20427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ace4985bf37a42bc248",
+    "url": "/@ramonak/react-excel/static/js/runtime-main.7aaf9204.js"
+  }
+]);
