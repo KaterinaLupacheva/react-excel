0cbb6a3d2ac16abb2455e1b9f3cf4bec195c95be
Katerina <katerinalupacheva@gmail.com>
2020-05-26 17:59:04 +0300

    Bugfix in create table body

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ export const ReactExcel = (props) => {
                 contentEditable
                 suppressContentEditableWarning={true}
                 onBlur={(e) => {
-                  updateSheet(e.currentTarget.textContent, i, idx);
+                  updateSheet(e.currentTarget.textContent, id, idx);
                 }}
               >
                 {cell}
